952b1d2cc16953d5e19630fffc32ae6fb4ba7702
Akram Khan <akrammiru@gmail.com>
2017-12-29 10:40:36 +0600

    Beginning project

---
+++ src/main/kotlin/com/akramkhan/vnoticeboard/VnoticeboardApplication.kt
@@ -0,0 +1,11 @@
+package com.akramkhan.vnoticeboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VnoticeboardApplication
+
+fun main(args: Array<String>) {
+    runApplication<VnoticeboardApplication>(*args)
+}
